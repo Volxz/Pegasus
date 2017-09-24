6419f875c2f622108086653c7a0d90b13b5dbcf8
Cpazek <Cpazek@gmail.com>
2017-09-24 14:18:46 -0700

    Added invalid login page for non LJCDS emails

---
+++ resources/views/invalid-login.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center" style="padding-top: 10vh;">Please Login with your @LJCDS.org Email</h1>
+                <a href="#"><button class="btn pri center-block" style="margin-top: 5vh;">Retry</button></a>
+            </div>
+        </div>
+    </div>
+    @endsection
