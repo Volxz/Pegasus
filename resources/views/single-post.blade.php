a043f7d9e28fd3ce777c348844cc99dad578d6df
Cpazek <Cpazek@gmail.com>
2017-09-25 14:14:57 -0700

    Added single post page

---
+++ resources/views/single-post.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+    .name {
+        color: #878088;
+        font-style: italic;
+        font-size: 10px;
+
+</style>
+<div class="background mh2">
+    {{--<img src="../img/404.png" width="800px" height="210px" class="logo center-block">--}}
+    <h1 class="text-center align-middle mainheader">Title</h1>
+
+
+</div>
+
+<br><br><br>
+<div class="container">
+    <div class="row">
+
+
+
+        <p class="name">Student Name</p>
+        <p class="name">Date: Date</p>
+        <hr style="border-top: 1px solid #0e3d08;">
+
+        <div>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat sem nec nunc laoreet luctus.
+                Aliquam consequat, ligula sit amet facilisis condimentum, magna nibh ultricies purus, quis semper ante
+                orci id magna. Etiam vulputate sed nulla sed bibendum. Maecenas pellentesque dapibus finibus. Ut mi
+                orci, tempus non magna sit amet, ultrices fringilla lacus. Pellentesque dapibus elit a nibh accumsan, at
+                posuere dui mollis. Nunc nec scelerisque quam. Aenean varius aliquam nulla, vel viverra felis dictum in.
+                Maecenas vitae porttitor est. Praesent placerat felis a lectus efficitur, id tincidunt quam vehicula.
+                Suspendisse eget tincidunt neque, sed efficitur eros. Integer sapien quam, maximus ac quam at, varius
+                vulputate arcu. Suspendisse dignissim dolor sed dui dignissim mattis. Aliquam pulvinar dolor sapien, ut
+                posuere est interdum vel. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+                cubilia Curae;
+
+                Vestibulum sodales porta nunc, at imperdiet libero posuere nec. Fusce quis mauris purus. Vestibulum
+                ullamcorper nisi vitae ultricies porttitor. Nam nec libero laoreet, vulputate est sed, vehicula sapien.
+                Duis at finibus ex. Aliquam maximus ipsum nec eleifend dictum. Nunc id finibus tortor. Pellentesque elit
+                enim, egestas non lacinia quis, congue quis risus. Suspendisse potenti. Aliquam erat volutpat. Nulla
+                tristique justo sollicitudin pharetra congue. Aenean vestibulum erat vitae pellentesque sodales. Duis
+                viverra tincidunt sem at finibus. Proin tempus vestibulum auctor. Nullam ac purus nibh.</p>
+
+
+
+
+
+        </div>
+
+    </div>
+</div>
+@endsection
