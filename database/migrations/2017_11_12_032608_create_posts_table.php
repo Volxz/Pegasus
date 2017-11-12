2418c5178fb03ed31b1c3fe051a65612e36c2d4e
volxz <ethan@exclnetworks.com>
2017-11-11 21:46:27 -0600

    Make post plural

--- database/migrations/2017_11_12_032608_create_posts_table.php
+++ database/migrations/2017_11_12_032608_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreatePostTable extends Migration {
+class CreatePostsTable extends Migration {
 
     public function up()
     {
